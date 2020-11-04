6a64ad10259b73fd7e297bf60c9fbe9db71e0df0
github username <hector.bale11@gmail.com>
2020-11-03 19:35:56 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // Your code here
+
+mapToDouble(sourceArray)
+// mapToSquare(sourceArray)
+
+function mapToNegativize(sourceArray) {
+    let r = []
+    for (let i = 0; i < sourceArray.length; i++ ) {
+      r.push(-1 * sourceArray[i])
+    }
+    return r
+  }
+  
+function mapToNoChange(sourceArray) {
+    let r = []
+    for (let i = 0; i < sourceArray.length; i++ ) {
+      r.push(sourceArray[i])
+    }
+    return r
+}
+
+function mapToDouble(sourceArray) {
+    let r = []
+    for (let i = 0; i < sourceArray.length; i++ ) {
+    r.push(2 * sourceArray[i])
+  }
+  return r
+}
+
+function mapToSquare(src) {
+    let r = []
+    for (let i = 0; i < src.length; i++ ) {
+      r.push(src[i] * src[i])
+    }
+    return r
+  }
+
+function reduceToTotal(src, startingPoint=0) {
+    let total = startingPoint
+    for (let i = 0; i < src.length; i++ ) {
+      total = total + src[i]
+    }
+    return total
+  }
+  
+  function reduceToAllTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (!src[i]) return false
+    }
+    return true
+  }
+
+  function reduceToAnyTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (src[i]) return true
+    }
+    return false
+  }
+// reduceToTotal(sourceArray, startingPoint)
+// reduceToAllTrue(sourceArray)
+// reduceToAnyTrue(sourceArray)
